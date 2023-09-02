f2a05bba45c5ed983e04cecf2507180741c86a72
yash-nigam <yashnigam@gmail.com>
2023-09-02 12:25:53 +0530

    Update rotate_using_reverse.py

--- Day6/rotate_using_reverse.py
+++ Day6/rotate_using_reverse.py
@@ -1,14 +1,26 @@
-def rev(arr, l, r):
-    c= len(arr)-1
-    while(l<r):
-        tmp=arr[r]
-        arr[r]=arr[l]
-        arr[l]=tmp
-        l+=1
-        r-=1
-    return arr
-a=[4,6,5,7,1,4,2,5]
-k=2
-print(rev(a,0,len(a)-1))
-print(rev(a,0,k-1))
-print(rev(a,k,len(a)-1))
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return a list of integers
+    def rev(self,A,l,r):
+        while(l<r):
+            temp=A[r]
+            A[r]=A[l]
+            A[l]=temp
+            l+=1
+            r-=1
+        return A
+
+    def rotate_via_rev(self,A,k):
+        A=self.rev(A,0, len(A)-1)
+        A=self.rev(A,0,k-1)
+        A=self.rev(A,k,len(A)-1)
+        return A
+
+    def solve(self, A, B):
+        if(len(A)<B):
+            B=B%len(A)
+            #print("B=",B)
+        if B>0:    
+            self.rotate_via_rev(A,B)
+        return A
